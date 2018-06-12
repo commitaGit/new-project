c06b569362bba159df0526c13b49b85f4333c6b2
commitaGit <hoze163@163.com>
2018-06-12 10:25:41 +0800

    first commit

---
+++ content/js/dashboard.js
@@ -0,0 +1,169 @@
+/*
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+var showControllersOnly = false;
+var seriesFilter = "";
+var filtersOnlySampleSeries = true;
+
+/*
+ * Populates the table identified by id parameter with the specified data and
+ * format
+ *
+ */
+function createTable(table, info, formatter, defaultSorts, seriesIndex) {
+    var tableRef = table[0];
+
+    // Create header and populate it with data.titles array
+    var header = tableRef.createTHead();
+    var newRow = header.insertRow(-1);
+    for (var index = 0; index < info.titles.length; index++) {
+        var cell = document.createElement('th');
+        cell.innerHTML = info.titles[index];
+        newRow.appendChild(cell);
+    }
+
+    var tBody;
+
+    // Create overall body if defined
+    if(info.overall){
+        tBody = document.createElement('tbody');
+        tBody.className = "tablesorter-no-sort";
+        tableRef.appendChild(tBody);
+        var newRow = tBody.insertRow(-1);
+        var data = info.overall.data;
+        for(var index=0;index < data.length; index++){
+            var cell = newRow.insertCell(-1);
+            cell.innerHTML = formatter ? formatter(index, data[index]): data[index];
+        }
+    }
+
+    // Create regular body
+    tBody = document.createElement('tbody');
+    tableRef.appendChild(tBody);
+
+    var regexp;
+    if(seriesFilter)
+        regexp = new RegExp(seriesFilter, 'i');
+
+    // Populate body with data.items array
+    for(var index=0; index < info.items.length; index++){
+        var item = info.items[index];
+        if((!regexp || filtersOnlySampleSeries && !info.supportsControllersDiscrimination || regexp.test(item.data[seriesIndex]))
+                &&
+                (!showControllersOnly || !info.supportsControllersDiscrimination || item.isController)){
+            var newRow = tBody.insertRow(-1);
+            for(var col=0; col < item.data.length; col++){
+                var cell = newRow.insertCell(-1);
+                cell.innerHTML = formatter ? formatter(col, item.data[col]) : item.data[col];
+            }
+        }
+    }
+
+    // Add support of columns sort
+    table.tablesorter({sortList : defaultSorts});
+}
+
+$(document).ready(function() {
+
+    // Customize table sorter default options
+    $.extend( $.tablesorter.defaults, {
+        theme: 'blue',
+        cssInfoBlock: "tablesorter-no-sort",
+        widthFixed: true,
+        widgets: ['zebra']
+    });
+
+    var data = {"OkPercent": 100.0, "KoPercent": 0.0};
+    var dataset = [
+        {
+            "label" : "KO",
+            "data" : data.KoPercent,
+               "color" : "red"
+        },
+        {
+            "label" : "OK",
+            "data" : data.OkPercent,
+            "color" : "blue"
+        }];
+    $.plot($("#flot-requests-summary"), dataset, {
+        series : {
+            pie : {
+                show : true,
+                radius : 1,
+                label : {
+                    show : true,
+                    radius : 3 / 4,
+                    formatter : function(label, series) {
+                        return '<div style="font-size:8pt;text-align:center;padding:2px;color:white;">'
+                            + label
+                            + '<br/>'
+                            + Math.round(series.percent)
+                            + '%</div>';
+                    },
+                    background : {
+                        opacity : 0.5,
+                        color : '#000'
+                    }
+                }
+            }
+        },
+        legend : {
+            show : true
+        }
+    });
+
+    // Creates APDEX table
+    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.9672790768296387, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)  ", "F (Frustration threshold)", "Label"], "items": [{"data": [0.9934760448521917, 500, 1500, "wechat_js_sdk"], "isController": false}, {"data": [0.9878640776699029, 500, 1500, "shop_setting"], "isController": false}, {"data": [0.8965032154340836, 500, 1500, "首页"], "isController": false}, {"data": [0.9919927022096088, 500, 1500, "user"], "isController": false}]}, function(index, item){
+        switch(index){
+            case 0:
+                item = item.toFixed(3);
+                break;
+            case 1:
+            case 2:
+                item = formatDuration(item);
+                break;
+        }
+        return item;
+    }, [[0, 0]], 3);
+
+    // Create statistics table
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 19758, 0, 0.0, 451.0, 536.0, 878.4099999999999, 325.4971087790975, 862.1167948221611, 80, 4138], "isController": false}, "titles": ["Label", "#Samples", "KO", "Error %", "90th pct", "95th pct", "99th pct", "Throughput", "KB/sec", "Min", "Max"], "items": [{"data": ["wechat_js_sdk", 4905, 0, 0.0, 319.40000000000055, 375.0, 529.7599999999984, 82.74151920514161, 55.99596954019838, 80, 1360], "isController": false}, {"data": ["shop_setting", 4944, 0, 0.0, 382.0, 439.0, 691.1000000000004, 82.01045036078627, 66.39322592684748, 97, 1722], "isController": false}, {"data": ["首页", 4976, 0, 0.0, 602.0, 761.4499999999989, 1150.3799999999974, 81.9755852457126, 680.6215095303207, 199, 4138], "isController": false}, {"data": ["user", 4933, 0, 0.0, 354.0, 401.0, 604.619999999999, 83.12410481085179, 62.2619027245345, 98, 1587], "isController": false}]}, function(index, item){
+        switch(index){
+            case 3:
+                item = item.toFixed(2) + '%';
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                item = item.toFixed(2);
+                break;
+        }
+        return item;
+    }, [[0, 0]], 0);
+
+    // Create error table
+    createTable($("#errorsTable"), {"supportsControllersDiscrimination": false, "titles": ["Type of error", "Number of errors", "% in errors", "% in all samples"], "items": []}, function(index, item){
+        switch(index){
+            case 2:
+            case 3:
+                item = item.toFixed(2) + '%';
+                break;
+        }
+        return item;
+    }, [[1, 1]]);
+});
